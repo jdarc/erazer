caa5832f5fa2c2ea3e48d7946a0aad6667365ce1
Jean d'Arc <jean.darc@thoughtworks.com>
2020-04-04 19:01:32 +0100

    Fixed several bugs and layout issues

--- background.js
+++ background.js
@@ -1,22 +1,21 @@
-const STORAGE_KEY = "com.zynaps.extensions.erazer.active";
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ [STORAGE_KEY]: true });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+  chrome.storage.sync.set({ isActive: true });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () =>
     chrome.declarativeContent.onPageChanged.addRules([
       {
-        conditions: [new chrome.declarativeContent.PageStateMatcher({})],
+        conditions: [new chrome.declarativeContent.PageStateMatcher()],
         actions: [new chrome.declarativeContent.ShowPageAction()]
       }
-    ]);
-  });
+    ])
+  );
 });
 
 chrome.downloads.onChanged.addListener(downloadDelta => {
-  if (downloadDelta.state && downloadDelta.state.current === "complete") {
-    chrome.storage.sync.get([STORAGE_KEY], data => {
-      if (data[STORAGE_KEY]) {
-        chrome.downloads.erase({});
+  if (downloadDelta.state && downloadDelta.state.current !== "in_progress") {
+    const downloadId = downloadDelta.id;
+    chrome.storage.sync.get(["isActive"], data => {
+      if (data.isActive) {
+        chrome.downloads.erase({ id: downloadId });
       }
     });
   }
--- popup.js
+++ popup.js
@@ -1,7 +1,5 @@
-const STORAGE_KEY = "com.zynaps.extensions.erazer.active";
-
 const toggler = document.getElementById("toggler");
 
-chrome.storage.sync.get([STORAGE_KEY], data => (toggler.checked = data[STORAGE_KEY]));
+chrome.storage.sync.get(["isActive"], data => (toggler.checked = data.isActive));
 
-toggler.onclick = element => chrome.storage.sync.set({ [STORAGE_KEY]: element.target.checked });
+toggler.onclick = element => chrome.storage.sync.set({ isActive: element.target.checked });
